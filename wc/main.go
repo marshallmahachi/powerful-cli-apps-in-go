cc317546a25c4de92d997bcc418719194f60fa5f
root <root@tino.localdomain>
2024-03-27 23:06:21 +0800

    add count-lines flag

--- wc/main.go
+++ wc/main.go
@@ -2,20 +2,29 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+
+	lines := flag.Bool("l", false, "Count lines")
+	flag.Parse()
+
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 
 	scanner := bufio.NewScanner(r)
 
-	scanner.Split(bufio.ScanWords)
+	if countLines {
+		scanner.Split(bufio.ScanLines)
+	} else {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	wc := 0
 
